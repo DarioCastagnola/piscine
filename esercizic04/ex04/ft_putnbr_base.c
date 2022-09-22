18dcb528ecd45dbefb2cd8dc05e9f0725fc74330
DarioCastagnola <112878659+DarioCastagnola@users.noreply.github.com>
2022-09-22 17:50:20 +0200

    Add files via upload

---
+++ esercizic04/ex04/ft_putnbr_base.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcastagn <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 10:16:56 by dcastagn          #+#    #+#             */
+/*   Updated: 2022/09/21 14:40:09 by dcastagn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+void	ft_putnbr
+{ 
+	-nb;
+}
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int i;
+	int nbr;
+
+	i = 0;
+	if (base[] == NULL || base[1]) || (base[i] == base[i - 1]
+		return (0)
+
